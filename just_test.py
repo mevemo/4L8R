5d497968d5c9aef14a94f27c4e8b0b65b199572d
Maik Heinrich <info@mevemo.de>
2021-08-02 14:12:51 +0200

    added age as input and gave the age as an output

--- just_test.py
+++ just_test.py
@@ -4,3 +4,8 @@
 # Here my Program tells the World, we exist
 print("Hello World")
 
+# Und jetzt macht er was neues, indem er den User nach seinem Alter fragt
+age = int(input("Bitte gib dein alter ein: "))
+
+# Und jetzt gibt er das alter aus.
+print("und du bist ", age, "Jahre alt.")
