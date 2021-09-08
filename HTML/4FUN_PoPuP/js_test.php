02b82ba0bbcd5766f266dfbc958a6be308af1501
M. Heinrich <76873680+mevemo@users.noreply.github.com>
2021-09-08 09:24:37 +0200

    Neue Website mit js

---
+++ HTML/4FUN_PoPuP/js_test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!-- saved from url=(0043)file:///Users/Mikesch/Downloads/tables.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title> HTML Basics </title>
+    <!-- <meta http-equiv="refresh" content="2; url=https://www.mozilla.org"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+    <script src="../js/pup.js" defer></script>
+
+    
+
+</head>
+
+<body>
+    <dl>
+        <dt> <a onmouseover="nhpup.popup('Bild zum Begriff: <br/><br/> <img src=&quot;guess_who.png&quot;/>', {'width': 200});">Begriff 1</a></h4></dt>
+        <dd> Beschreibung des Begriffs</dd>
+    </dl>
+</body></html>
