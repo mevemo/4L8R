2d9ed698de7d049aa4f306f0427c80f5b4603281
M. Heinrich <76873680+mevemo@users.noreply.github.com>
2021-09-09 09:51:14 +0200

    Starting Workshop Thursday

---
+++ HTML/WORKSHOP/seite_01.php
@@ -0,0 +1,23 @@
+<?php
+    require_once "inc.head.html";
+?>
+<header>
+Inhalt Seite 01
+</header>
+
+<section>
+
+<dl>
+        <dt> <a onmouseover="nhpup.popup('So habe ich das gemacht: <br/><br/> <img src=&quot;guess_who.png&quot;/>', 
+                {'width': 200});">Begriff 1</a></h4></dt>
+        <dd> Beschreibung des Begriffs</dd>
+    </dl>
+</section>
+
+<footer>
+
+</footer>
+
+<?php
+    require_once "inc.foot.html";
+?>
---
+++ HTML/WORKSHOP/seite_02.php
@@ -0,0 +1,18 @@
+<?php
+    require_once "inc.head.html";
+?>
+<header>
+    
+</header>
+
+<section>
+Inhalt Seite 02
+</section>
+
+<footer>
+
+</footer>
+
+<?php
+    require_once "inc.foot.html";
+?>
